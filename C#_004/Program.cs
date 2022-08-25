20f605c95321cc8285519c7b3bfcd4483f9f58d6
Roman <romangushchintlt@gmail.com>
2022-08-25 16:36:13 +0400

    Решил четвертую задачу

--- C#_004/Program.cs
+++ C#_004/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 8
+// Первое условие
+// int a = 1;
+// int n = 5;
+// for (int i = a; i <= n; i++)
+// {
+//     if (i % 2 == 0) 
+//     {
+//         Console.Write(i + " ");
+//     }
+// }
+// Console.WriteLine();
+
+// Второе условие
+int a = 1;
+int n = 8;
+for (int i = a; i <= n; i++)
+{
+    if (i % 2 == 0) 
+    {
+        Console.Write(i + " ");
+    }
+}
+Console.WriteLine();
+
+
+
