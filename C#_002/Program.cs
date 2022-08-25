ff71c2007b97956d6c88cf4a57b98b872df63606
Roman <romangushchintlt@gmail.com>
2022-08-25 16:02:03 +0400

    Выполнил вторую задачу

--- C#_002/Program.cs
+++ C#_002/Program.cs
@@ -1,45 +1,35 @@
 ﻿//Задача 4
 // Первое условие
 
-int a = 1;
-int b = 2;
-int c = 6;
-int d = 8;
-int e = 4;
+// int a = 5;
+// int b = 3;
+// int c = 7;
+// int max = a;
+// if (a > max ) max = a;
+// if (b > max ) max = b;
+// if (c > max ) max = c;
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+
+//Второе условие
+// int a = 44;
+// int b = 5;
+// int c = 78;
+// int max = a;
+// if (a > max ) max = a;
+// if (b > max ) max = b;
+// if (c > max ) max = c;
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+//Третье условие
+int a = 22;
+int b = 3;
+int c = 9;
 int max = a;
 if (a > max ) max = a;
 if (b > max ) max = b;
 if (c > max ) max = c;
-if (d > max ) max = d;
-if (e > max ) max = e;
 Console.Write("max = ");
 Console.WriteLine(max);
-
-
-//Второе условие
-/*
-int a = 2;
-int b = 10;
-
-int max = a;
-
-if (a > b) max = a;
-else max = b;
-
-Console.Write("max = ");
-Console.WriteLine(max);
-*/
-
-//Третье условие
-/*
-int a = -9;
-int b = -3;
-
-int max = a;
-
-if (a > b) max = a;
-else max = b;
-
-Console.Write("max = ");
-Console.WriteLine(max);
-*/
