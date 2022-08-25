0c8fe3f3833a7d010b09c722b93b2ce25a16f722
Roman <romangushchintlt@gmail.com>
2022-08-25 15:48:19 +0400

    Изменил код в первой задаче

--- C#_001/Program.cs
+++ C#_001/Program.cs
@@ -1,40 +1,32 @@
 ﻿//Задача 2
 // Первое условие
-/*
-int a = 5;
-int b = 7;
 
-int max = a;
+// int a = 5;
+// int b = 7;
+// int max = a;
+// if (a > max ) max = a;
+// if (b > max ) max = b;
+// Console.Write("max = ");
+// Console.WriteLine(max);
 
-if (a > b) max = a;
-else max = b;
-
-Console.Write("max = ");
-Console.WriteLine(max);
-*/
 
 //Второе условие
-/*
-int a = 2;
-int b = 10;
 
-int max = a;
+// int a = 2;
+// int b = 10;
+// int max = a;
+// if (a > max ) max = a;
+// if (b > max ) max = b;
+// Console.Write("max = ");
+// Console.WriteLine(max);
 
-if (a > b) max = a;
-else max = b;
-
-Console.Write("max = ");
-Console.WriteLine(max);
-*/
 
 //Третье условие
+
 int a = -9;
 int b = -3;
-
 int max = a;
-
-if (a > b) max = a;
-else max = b;
-
+if (a > max ) max = a;
+if (b > max ) max = b;
 Console.Write("max = ");
 Console.WriteLine(max);
--- C#_002/Program.cs
+++ C#_002/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4
+// Первое условие
+
+int a = 1;
+int b = 2;
+int c = 6;
+int d = 8;
+int e = 4;
+int max = a;
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+if (d > max ) max = d;
+if (e > max ) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
+
+
+//Второе условие
+/*
+int a = 2;
+int b = 10;
+
+int max = a;
+
+if (a > b) max = a;
+else max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+*/
+
+//Третье условие
+/*
+int a = -9;
+int b = -3;
+
+int max = a;
+
+if (a > b) max = a;
+else max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+*/
